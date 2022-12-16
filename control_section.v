8707ef12284028d3f1350c334ef4e80c9c59014a
bsilverriverd <40031729+bsilverriverd@users.noreply.github.com>
2022-12-16 13:15:49 +0900

    Add files via upload

--- control_section.v
+++ control_section.v
@@ -39,7 +39,7 @@ module control_section(
     
     assign b1 = step[0];
     assign iar_en = clk_en & step[0];
-    assign ram_en = clk_en & step[1];
+    assign ram_en = clk_en & (step[1] | load_rb_set);
     assign acc_en = clk_en & (step[3] | alu_rb_set);
     assign r0_en = (clk_en & RegA_en & ra_sel[0]) | (clk_en & RegB_en & rb_sel[0]);
     assign r1_en = (clk_en & RegA_en & ra_sel[1]) | (clk_en & RegB_en & rb_sel[1]);
@@ -47,10 +47,10 @@ module control_section(
     assign r3_en = (clk_en & RegA_en & ra_sel[3]) | (clk_en & RegB_en & rb_sel[3]);
     
     assign ir_set = clk_set & step[1];
-    assign mar_set = clk_set & step[0];
+    assign mar_set = clk_set & (step[0] | load_ra_en | store_ra_en);
     assign iar_set = clk_set & step[2];
     assign acc_set = clk_set & (step[1] | alu_ra_en) ;
-    assign ram_set = clk_set;
+    assign ram_set = clk_set & store_rb_en;;
     assign tmp_set = clk_set & alu_rb_en;
     assign r0_set = clk_set & RegB_set & rb_sel[0];
     assign r1_set = clk_set & RegB_set & rb_sel[0];
@@ -67,12 +67,26 @@ module control_section(
     assign alu_ra_en = ir[7] & step[4];
     assign alu_rb_set = ir[7] & step[5] & !(ir[6] & ir[5] & ir[4]);
     
-    assign RegB_en = ir[7] & step[3];
-    assign RegA_en = ir[7] & step[4];
-    assign RegB_set = ir[7] & step[5] & !(ir[6] & ir[5] & ir[4]);
+    assign RegB_en = alu_rb_en | store_rb_en;
+    assign RegA_en = alu_ra_en | load_ra_en | store_ra_en;
+    assign RegB_set = alu_rb_set | load_rb_set;
         
     operand_decoder ra_en(.in(ir[1:0]), .out(ra_sel));
     operand_decoder rb_en(.in(ir[3:2]), .out(rb_sel));
+    
+    wire [7:0] nonalu_dec_out;
+    nonalu_decoder nonalu_dec(.in(ir[6:4]), .out(nonalu_dec_out));
+    
+    wire load, store;
+    wire load_ra_en, load_rb_set;
+    wire store_ra_en, store_rb_en;
+    
+    assign load = ir[7] & nonalu_dec_out[0];
+    assign store = ir[7] & nonalu_dec_out[1];
+    assign load_ra_en = step[3] & load;
+    assign load_rb_set = step[4] & load;
+    assign store_ra_en = step[3] & store;
+    assign store_rb_en = step[4] & store; 
     
 endmodule
 
@@ -88,4 +102,24 @@ module operand_decoder(
     and a1(out[1], in[0], not_in[1]);
     and a2(out[2], not_in[0], in[1]);
     and a3(out[3], in[0], in[1]);
+endmodule
+
+module non_alu_decoder(
+    input[2:0] in,
+    output[7:0] out
+);
+    wire [2:0] not_in;
+    not(not_in[0], in[0]);
+    not(not_in[1], in[1]);
+    not(not_in[2], in[2]);
+    
+    and a0(out[0], not_in[0], not_in[1], not_in[2]);
+    and a1(out[1], in[0], not_in[1], not_in[2]);
+    and a2(out[2], not_in[0], in[1], not_in[2]);
+    and a3(out[3], in[0], in[1], not_in[2]);
+    and a4(out[4], not_in[0], not_in[1], in[2]);
+    and a5(out[5], in[0], not_in[1], in[2]);
+    and a6(out[6], not_in[0], in[1], in[2]);
+    and a7(out[7], in[0], in[1], in[2]);
+
 endmodule
